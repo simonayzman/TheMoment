036dfe1d3def4130e974ce76d3e57611b965b7ea
Simon Ayzman <simonayzman@gmail.com>
2016-09-10 19:55:55 -0400

    Added thunk to middleware

--- configureStore.js
+++ configureStore.js
@@ -4,7 +4,10 @@ import createLogger from 'redux-logger';
 import reducer from './reducer';
 
 const logger = createLogger();
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  thunk,
+  logger,
+)(createStore);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleware(reducer, initialState);
