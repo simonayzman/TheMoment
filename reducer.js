9c7f5fe941f28ed1b3e8f2190db1554deef71acd
Simon Ayzman <simonayzman@gmail.com>
2016-09-10 23:47:20 -0400

    Updated reducer with better naming

--- reducer.js
+++ reducer.js
@@ -4,14 +4,14 @@ const {
 } = constants;
 
 const initialState = {
-  moments: 0, // get actual number from persistent store?
+  count: 0, // get actual number from persistent store?
 };
 
 export default function moment(state = initialState, action) {
   switch (action.type) {
 
     case LIVE_IN_THE_MOMENT:
-      return { ...state, moments: state.moments+1 };
+      return { ...state, count: state.count+1 };
 
     default:
       return state;
