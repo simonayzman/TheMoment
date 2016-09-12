6a1f198129a9dbb40c0ca99a24ed199b9c828671
Simon Ayzman <simonayzman@gmail.com>
2016-09-12 16:59:20 -0400

    Updated intro page with new logo/tagline

--- components/IntroPage.js
+++ components/IntroPage.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet,  Text,  View,  Image, TouchableOpacity,  Modal} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Modal,
+  Dimensions,
+} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+
+const MARGIN = 15;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 150,
     backgroundColor: 'rgba(0,0,0,0)', // so text doesn't have a bg
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   h1: {
     fontSize: 55,
@@ -30,35 +42,58 @@ const styles = StyleSheet.create({
     fontSize: 25,
     textAlign: 'center',
     margin: 10,
-    marginBottom: 25,
-    borderRadius: 5,
+    borderRadius: 25,
     borderWidth: 0.1,
-    borderColor: 'rgba(0,0,0,0.2)',
-    textAlign: 'center',
+    borderColor: 'rgb(255,255,255)',
     borderWidth: 1,
     padding: 5,
-    fontFamily:'Avenir',
-    color: 'white',
     backgroundColor: 'rgba(0,0,0,0.1)',
     width: 250,
-    marginTop: 250
+    textAlign: 'center',
+    fontFamily:'Avenir',
+    color: 'white',
+    overflow: 'hidden',
+//    marginBottom: 100,
   },
-  bg: { flex: 1, resizeMode: 'stretch'},
-  bgwrap:{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }
-
+  bg: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    resizeMode: 'stretch',
+    width: null,
+    height: null,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  momentLogoWithTagline: {
+    resizeMode: 'contain',
+    width: SCREEN_WIDTH - MARGIN * 2,
+    margin: MARGIN,
+//    marginTop: bo,
+  }
 });
 
 
 export default class IntroPage extends Component {
   render() {
     return (
-      <View>
-        <View style={styles.bgwrap}><Image source={require('../assets/bg.jpg')} style={styles.bg} /></View>
-        <View style={styles.container}>
-          <Text style={styles.h1}>THE MOMENT</Text>
-          <Text style={styles.h2}>LIVE IN THE MOMENT</Text>
-          <TouchableOpacity onPress={Actions.mainPage}><Text style={styles.button}>GET STARTED</Text></TouchableOpacity>
-        </View>
+      <View style={styles.container}>
+        <Image
+          source={require('../assets/bg.jpg')}
+          style={styles.bg}
+        >
+          <Image
+            source={require('../assets/moment-logo-with-tagline.png')}
+            style={styles.momentLogoWithTagline}
+          />
+          <TouchableOpacity onPress={Actions.mainPage}>
+            <Text style={styles.button}>
+              GET STARTED
+            </Text>
+          </TouchableOpacity>
+        </Image>
       </View>
     );
   }
