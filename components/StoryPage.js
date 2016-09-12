4a6187493f83d3eec4d8db37023b75c2c2ce9eb4
Simon Ayzman <simonayzman@gmail.com>
2016-09-12 15:38:51 -0400

    Added more stories to story page

--- components/StoryPage.js
+++ components/StoryPage.js
@@ -64,16 +64,46 @@ const stories = [
     name:"Sarah G.",
   },
   {
-    description: "Living in the moment! Thanks Tide!",
+    description: "Living in the moment! Thanks Raid!",
     image: require('../assets/stories/wedding.png'),
     timePassed: "2h",
     name:"Kim S.",
   },
   {
-    description: "Living in the moment! Thanks Tide!",
-    image: require('../assets/stories/beach.png'),
+    description: "Living in the moment! Thanks Miralax!",
+    image: require('../assets/stories/hillary.jpg'),
     timePassed: "6h",
+    name:"Hill C.",
+  },
+  {
+    description: "Living in the moment! Thanks Tropicana!",
+    image: require('../assets/stories/beach.png'),
+    timePassed: "13h",
+    name:"Jenny L.",
+  },
+  {
+    description: "Living in the moment! Thanks Viagra!",
+    image: require('../assets/stories/bed.jpg'),
+    timePassed: "14h",
+    name:"Mark M.",
+  },
+  {
+    description: "Living in the moment! Thanks Tide!",
+    image: require('../assets/stories/pilot.jpg'),
+    timePassed: "1d",
+    name:"Bob B.",
+  },
+  {
+    description: "Living in the moment! Thanks Tropicana!",
+    image: require('../assets/stories/bus.jpg'),
+    timePassed: "1d",
     name:"Jess H.",
+  },
+  {
+    description: "Living in the moment! Thanks Miralax!",
+    image: require('../assets/stories/students.jpg'),
+    timePassed: "1d",
+    name:"John S.",
   },
 ];
 
